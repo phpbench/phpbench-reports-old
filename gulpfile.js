c61d41b4376952fb1b8feb0915713b0bad04882b
dantleech <dan.t.leech@gmail.com>
2017-11-26 20:35:55 +0100

    Added web assets

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 
 gulp.task('default', function(){
   return gulp.src([
-      'node_modules/semantic-ui-css/**.css',
-      'node_modules/chart.js/dist/*.js'
+      'node_modules/semantic-ui-css/**.min.css',
+      'node_modules/chart.js/dist/*.min.js'
   ]).pipe(gulp.dest('web/assets'))
 });
