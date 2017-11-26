0c876240764e751271da2c26d6985492847dd353
dantleech <dan.t.leech@gmail.com>
2017-11-26 16:51:09 +0100

    Handle json errors

---
+++ src/Web/Middleware/SecurityMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Phpbench\Reports\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Exception;
+
+class JsonErrorMiddleware
+{
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    {
+        try {
+            return $next($request, $response);
+        } catch (Exception $exception) {
+            if ($request->getHeader('content-type')[0] !== 'application/json') {
+                throw $exception;
+            }
+            $response = $response->withStatus('500');
+
+            $response->getBody()->write(json_encode([
+                'exception' => get_class($exception),
+                'message' => $exception->getMessage(),
+            ]));
+
+            return $response;
+        }
+    }
+}
