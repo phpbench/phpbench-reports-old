3e10342e07fc500364c6a3c789ed81699867ab84
dantleech <dan.t.leech@gmail.com>
2017-11-26 09:22:23 +0100

    Show stats

--- src/Web/Handler/VariantHandler.php
+++ src/Web/Handler/VariantHandler.php
@@ -37,6 +37,7 @@ class VariantHandler
             'iterations' => $iterations,
             'benchmark' => $class,
             'subject' => $subject,
+            'suite' => $suite,
         ]));
 
         return $response;
