6a5ed6c339e0fbc2542c282d4eb082ca2feeeabf
dantleech <dan.t.leech@gmail.com>
2017-11-26 16:19:24 +0100

    Refactored£

--- tests/Context/ApiContext.php
+++ tests/Context/ApiContext.php
@@ -12,15 +12,29 @@ use Symfony\Component\BrowserKit\Client;
 class ApiContext extends RawMinkContext implements Context
 {
     /**
-     * @Given I post the following payload to :url:
+     * @When I submit the :arg1 payload to the variants endpoint
      */
-    public function iPostTheFollowingPayloadTo($url, PyStringNode $payload)
+    public function iSubmitThePayloadToTheVariantsEndpoint($payloadName)
     {
-        $this->getClient()->request('POST', $url, [], [], [], (string) $payload);
+        $this->submitPayload($payloadName, '/api/v1/suite');
+    }
+
+    /**
+     * @When I submit the :payloadName payload to the iterations endpoint
+     */
+    public function iSubmitThePayloadToTheIterationsEndpoint($payloadName)
+    {
+        $this->submitPayload($payloadName, '/api/v1/iterations');
     }
 
     private function getClient(): Client
     {
         return $this->getSession()->getDriver()->getClient();
+    }
+
+    private function submitPayload($payloadName, string $url): void
+    {
+        $payload = file_get_contents(__DIR__ . '/payloads/' . $payloadName);
+        $this->getClient()->request('POST', $url, [], [], [], (string) $payload);
     }
 }
